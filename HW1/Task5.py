40c5a0ec2861f84a6d4b8d98c8646dcbf078c219
Jane <spring1981@mail.ru>
2022-12-02 09:18:09 +0300

    Solution added

--- HW1/Task5.py
+++ HW1/Task5.py
@@ -1,3 +1,12 @@
 # 5.	Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними
 # в 2D пространстве.
 
+x1 = float(input('Enter the first coordinate of A point: '))
+y1 = float(input('Enter the second coordinate of A point: '))
+
+x2 = float(input('Enter the first coordinate of B point: '))
+y2 = float(input('Enter the first coordinate of B point: '))
+
+lengthAB = ((x2 - x1) + (y2 - y1))**0.5
+
+print(round(lengthAB, 2))
